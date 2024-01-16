103722a4c317b90b3de2b9e083228ffee67bb9cc
Daniel <danieldoleski02@gmail.comm>
2024-01-16 20:02:25 -0300

    a

---
+++ src/Aula04/Controllers/UsuáriosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Aula04.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Aula04.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuáriosController : ControllerBase
+    {
+        public readonly ValidadorDeUsuario _validadorDeUsuario;
+
+        public UsuáriosController()
+        {
+            _validadorDeUsuario = new ValidadorDeUsuario();
+        }
+
+        [HttpPost]
+        public ActionResult AdicionarUsuario(Usuario usuario)
+        {
+            string validacaoUsuario = _validadorDeUsuario.Validar(usuario);
+            if (string.IsNullOrEmpty(validacaoUsuario) is false)
+            {
+                return BadRequest(validacaoUsuario);
+            }
+
+            return Ok();
+        }
+
+    }
+}
---
+++ src/Aula04/Models/Usuarios.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using FluentValidation;
+
+namespace Aula04.Models
+{
+    public class Usuario
+    {
+        public string? ID { get; set; }
+
+        public string Email { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Estado { get; set; }
+
+        public string CEP { get; set;}
+    }
+
+    public class ValidadorComFluent : AbstractValidator<Usuario>
+    {
+        public ValidadorComFluent()
+        {
+            RuleFor( usuario => usuario.ID ).NotEmpty().Length(5);
+            RuleFor( usuario ) 
+        }
+    }
+
+    public class ValidadorDeUsuario
+    {
+        public string Validar(Usuario usuario)
+        {
+            string erro = string.Empty;
+
+            if (string.IsNullOrEmpty(usuario.ID))
+            {
+                erro = "Não pode passar ID vazio";
+                return erro;
+            }
+
+            if (string.IsNullOrEmpty(usuario.Email))
+            {
+                if (usuario.Email.Contains("@"))
+                {
+                    if (usuario.Email.Length <= 7)
+                    {
+                        erro = "Deve ter no mínimo 7 caracteres";
+                        return erro;
+                    }
+                }
+                else
+                {
+                    erro = "Não pode passar email vazio";
+                    return erro;
+                }
+            }
+            else
+            {
+                erro = "Deve seguir o formato de email";
+                return erro;
+            }
+            
+
+            return erro;
+        }
+    }
+}
